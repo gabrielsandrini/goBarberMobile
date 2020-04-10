a5a7e1944377e65311dcb933b9dff99c14c6d27e
Gabriel Sandrini <gabriel.sandrini@outlook.com>
2020-04-10 08:36:31 -0300

    Input and Button component

--- src/pages/SignIn/index.js
+++ src/pages/SignIn/index.js
@@ -1,12 +1,20 @@
 import React from 'react';
-import {Text} from 'react-native';
+import { Text } from 'react-native';
 
 import BackGround from '~/components/Background';
+import Input from '~/components/Input';
+import Button from '~/components/Button';
 
 export default function SignIn() {
   return (
     <BackGround>
       <Text>Sign In</Text>
+      <Input
+        icon="call"
+        placeholder="Digite seu nome"
+        style={{ marginTop: 30 }}
+      />
+      <Button>Entrar</Button>
     </BackGround>
   );
 }
