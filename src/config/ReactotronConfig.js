d164634e3626acab23af53d0dcd972fa6a7ec22b
Gabriel Sandrini <gabriel.sandrini@outlook.com>
2020-04-10 12:00:29 -0300

    configuring redux

--- src/config/ReactotronConfig.js
+++ src/config/ReactotronConfig.js
@@ -1,13 +1,15 @@
 import Reactotron from 'reactotron-react-native';
 import { reactotronRedux } from 'reactotron-redux';
 import reactotronSaga from 'reactotron-redux-saga';
+import AsyncStorage from '@react-native-community/async-storage';
 
 if (__DEV__) {
-  const tron = Reactotron.configure()
+  const tron = Reactotron.setAsyncStorageHandler(AsyncStorage)
+    .configure()
     .useReactNative()
     .use(reactotronRedux())
     .use(reactotronSaga())
-    .connect({ host: '192.168.0.10' });
+    .connect();
 
   tron.clear();
 
