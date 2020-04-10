f20a7052c1e7ab7fd89ae3f24afd23bb7981b9cf
Gabriel Sandrini <gabriel.sandrini@outlook.com>
2020-04-10 06:10:36 -0300

    Background color

--- src/pages/SignIn/index.js
+++ src/pages/SignIn/index.js
@@ -1,12 +1,12 @@
 import React from 'react';
-import {View, Text} from 'react-native';
+import {Text} from 'react-native';
 
-// import { Container } from './styles';
+import BackGround from '~/components/Background';
 
 export default function SignIn() {
   return (
-    <View>
+    <BackGround>
       <Text>Sign In</Text>
-    </View>
+    </BackGround>
   );
 }
