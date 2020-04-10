553ce57755fd5371f6f31442d6d53bcc944f4ea7
Gabriel Sandrini <gabriel.sandrini@outlook.com>
2020-04-10 05:15:07 -0300

    babel-plugin-root-import

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import {View} from 'react-native';
-
+import Teste from '~/teste.js';
 // import { Container } from './styles';
 
 export default function src() {
-  return <View />;
+  return <Teste />;
 }
