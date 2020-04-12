ad9a381eab6c571d5abe87e950784efcef3f2c5e
Gabriel Sandrini <gabriel.sandrini@outlook.com>
2020-04-12 17:44:53 -0300

    Private routes + Tab navigator

--- src/routes.js
+++ src/routes.js
@@ -1,28 +1,52 @@
 import 'react-native-gesture-handler';
 import React from 'react';
-import {NavigationContainer} from '@react-navigation/native';
-import {createStackNavigator} from '@react-navigation/stack';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import PropTypes from 'prop-types';
 
 import SignIn from '~/pages/SignIn';
 import SignUp from '~/pages/SignUp';
+import Dashboard from './pages/Dashboard';
 
 const Stack = createStackNavigator();
+const Tab = createBottomTabNavigator();
 
-export default function App() {
+function Sign() {
+  return (
+    <Stack.Navigator headerMode="none">
+      <Stack.Screen name="SignIn" component={SignIn} />
+      <Stack.Screen name="SignUp" component={SignUp} />
+    </Stack.Navigator>
+  );
+}
+
+function App() {
+  return (
+    <Tab.Navigator>
+      <Tab.Screen name="DashBoard" component={Dashboard} />
+    </Tab.Navigator>
+  );
+}
+
+export default function Routes({ signed }) {
   return (
     <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen
-          name="SignIn"
-          component={SignIn}
-          options={{headerShown: false}}
-        />
-        <Stack.Screen
-          name="SignUp"
-          component={SignUp}
-          options={{headerShown: false}}
-        />
+      <Stack.Navigator headerMode="none">
+        {signed ? (
+          <Stack.Screen name="App" component={App} />
+        ) : (
+          <Stack.Screen name="Sign" component={Sign} />
+        )}
       </Stack.Navigator>
     </NavigationContainer>
   );
 }
+
+Routes.propTypes = {
+  signed: PropTypes.bool,
+};
+
+Routes.defaultProps = {
+  signed: false,
+};
